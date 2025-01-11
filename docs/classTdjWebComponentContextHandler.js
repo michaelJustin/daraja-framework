395bd455afd6d28774be124fa25d54157e573ed0
Michael Justin <michael@mikejustin.com>
2025-01-11 18:42:48 +0100

    Update API docs

--- docs/classTdjWebComponentContextHandler.js
+++ docs/classTdjWebComponentContextHandler.js
@@ -2,9 +2,9 @@ var classTdjWebComponentContextHandler =
 [
     [ "TdjWebComponentContextHandler", "classTdjWebComponentContextHandler.html#a530756850dbf7a1a24ca2423dd857a36", null ],
     [ "~TdjWebComponentContextHandler", "classTdjWebComponentContextHandler.html#ac7ae07481bdf57ea3bd4374a0587352a", null ],
-    [ "AddFilterWithMapping", "classTdjWebComponentContextHandler.html#a38e7bbe2ae609ba669730e0c3827d577", null ],
-    [ "AddWebComponent", "classTdjWebComponentContextHandler.html#a277caa51f38d1420fe229b350de75b7b", null ],
+    [ "AddWebComponent", "classTdjWebComponentContextHandler.html#abda1ca99ff0cf8025fd010d0a6189cb9", null ],
     [ "AddWebComponent", "classTdjWebComponentContextHandler.html#a8578fec0b240ffcdf853a821948d6157", null ],
+    [ "AddWebFilter", "classTdjWebComponentContextHandler.html#a2db7d8cd6ad5daa772ae443dc7cea8d6", null ],
     [ "AddWebFilter", "classTdjWebComponentContextHandler.html#a9461d1917c5c0155682c9b9f25730c6e", null ],
     [ "DoHandle", "classTdjWebComponentContextHandler.html#af0c5c36f2b2019fcb915fd50b46dfe74", null ],
     [ "Handle", "classTdjWebComponentContextHandler.html#aac1c42962e5a3a2a16cdabf79c6d5924", null ]
