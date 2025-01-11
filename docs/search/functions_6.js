ce7308bcbb15fdeb4a322e081789e1ef87829e91
Michael Justin <michael@mikejustin.com>
2025-01-11 16:21:57 +0100

    Update API docs to 3.0-MS3

--- docs/search/functions_6.js
+++ docs/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['init_0',['Init',['../classTdjContext.html#a61788bb9e72553e1a3fb4d383a315a6b',1,'TdjContext::Init()'],['../classTdjGenericWebComponent.html#a4e0eb0bfa112b77285c91e2a783630e5',1,'TdjGenericWebComponent::Init()']]],
+  ['init_0',['Init',['../classTdjContext.html#a61788bb9e72553e1a3fb4d383a315a6b',1,'TdjContext::Init()'],['../classTdjGenericWebComponent.html#a4e0eb0bfa112b77285c91e2a783630e5',1,'TdjGenericWebComponent::Init(const IWebComponentConfig Config)'],['../classTdjGenericWebComponent.html#acc86d67fcfe29d9c71531fccc956c868',1,'TdjGenericWebComponent::Init()'],['../classTdjGenericWebFilter.html#a548ef13ea7c6d141754fa27e14bae29a',1,'TdjGenericWebFilter::Init(const IWebFilterConfig Config)'],['../classTdjGenericWebFilter.html#aceeb0b1213e29cc5c09536ca0d685f2e',1,'TdjGenericWebFilter::Init()']]],
   ['isstarted_1',['IsStarted',['../classTdjLifeCycle.html#a5f28103039a0b01fd33cbe20155674e6',1,'TdjLifeCycle']]],
   ['isstopped_2',['IsStopped',['../classTdjLifeCycle.html#a8ec8178ac70d4e5b45b9ea2e5b046fc1',1,'TdjLifeCycle']]]
 ];
