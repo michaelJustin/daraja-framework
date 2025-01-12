46bfd7b9d19842afbd471e28c7a5878a15ba785c
Michael Justin <michael@mikejustin.com>
2025-01-12 18:43:07 +0100

    Rebuild docs

--- docs/classTdjWebComponentContextHandler.js
+++ docs/classTdjWebComponentContextHandler.js
@@ -2,6 +2,8 @@ var classTdjWebComponentContextHandler =
 [
     [ "TdjWebComponentContextHandler", "classTdjWebComponentContextHandler.html#a530756850dbf7a1a24ca2423dd857a36", null ],
     [ "~TdjWebComponentContextHandler", "classTdjWebComponentContextHandler.html#ac7ae07481bdf57ea3bd4374a0587352a", null ],
+    [ "Add", "classTdjWebComponentContextHandler.html#a05c9ce96738c3f676a7ecca9b35d5b83", null ],
+    [ "Add", "classTdjWebComponentContextHandler.html#a74657f922fa2e5a3bad62dfc531ac100", null ],
     [ "AddWebComponent", "classTdjWebComponentContextHandler.html#a33d9eb537de88795497a683495dc9569", null ],
     [ "AddWebComponent", "classTdjWebComponentContextHandler.html#a84471daf7b1587f91109682c51c01b53", null ],
     [ "AddWebFilter", "classTdjWebComponentContextHandler.html#aa8e298da47668098e3bf8b32799f3796", null ],
