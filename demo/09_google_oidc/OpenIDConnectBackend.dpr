ab9225a67e6b5d04118d2f03526e7ef608a84560
Michael Justin <michael@mikejustin.com>
2025-04-24 08:23:44 +0700

    327-prepare-301-release
    Copyright (C)

--- demo/09_google_oidc/OpenIDConnectBackend.dpr
+++ demo/09_google_oidc/OpenIDConnectBackend.dpr
@@ -1,7 +1,7 @@
 (*
 
     Daraja HTTP Framework
-    Copyright (C) Michael Justin
+    Copyright (c) Michael Justin
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
--- demo/common/djFileUploadHelper.pas
+++ demo/common/djFileUploadHelper.pas
@@ -1,5 +1,5 @@
 (*
-   Copyright (C) Michael Justin
+   Copyright (c) Michael Justin
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
