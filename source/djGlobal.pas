f463d399081d17329696dc8d4837474047666575
Michael Justin <michael@mikejustin.com>
2025-05-24 17:18:59 +0200

    start 3.0.5-SNAPSHOT

--- source/djGlobal.pas
+++ source/djGlobal.pas
@@ -35,7 +35,7 @@ interface
 // {$i IdCompilerDefines.inc}
 
 const
-  DWF_SERVER_VERSION = '3.0.4';
+  DWF_SERVER_VERSION = '3.0.5-SNAPSHOT';
   DWF_SERVER_FULL_NAME = 'Daraja HTTP Framework ' + DWF_SERVER_VERSION;
   DWF_SERVER_COPYRIGHT = 'Copyright (c) Michael Justin';
 
